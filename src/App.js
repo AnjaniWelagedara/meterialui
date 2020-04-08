2167723fbe21bdc1545bdc0b63c89bc150e1e550
AnjaniWelagedara <anjanwelagedara3@gmail.com>
2020-04-08 18:42:25 +0530

    Log in UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
-function App() {
+
+const useStyles = makeStyles((theme) => ({
+
+    root: {
+    '& > *': {
+      margin: theme.spacing(2),
+        width:'80ch',
+
+    },
+
+      bullet: {
+          display: 'inline-block',
+          margin: '0 2px',
+          transform: 'scale(0.8)',
+      },
+      title: {
+          fontSize: 14,
+      },
+      pos: {
+          marginBottom: 12,
+      },
+
+
+
+
+
+  },
+}));
+
+export default function ContainedButtons() {
+  const classes = useStyles();
+    const bull = <span className={classes.bullet}>•</span>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={classes.root}>
+
+          <Card className={classes.card}>
+
+              <form className={classes.root} noValidate autoComplete="off">
+                  <h1>User Login</h1>
+                  <TextField id="standard-basic" label="Username" />
+                  <TextField id="standard-basic" label="Password" />
+
+                  <Button variant="contained" color="primary">
+                      Primary
+                  </Button>
+
+              </form>
+
+
+
+              <CardContent>
+
+
+
+
+
+
+
+
+
+
+
+              </CardContent>
+
+          </Card>
+
+
+
+
+      </div>
   );
 }
 
-export default App;
+
